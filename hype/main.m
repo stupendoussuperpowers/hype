688c7da5d550a4284ff9e44e1be0e8da7556fdac
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2024-08-10 15:28:20 +0530

    Initial Commit

---
+++ hype/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hype
+//
+//  Created by Sanchit Sahay on 10/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
